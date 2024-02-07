6e9c96e90b2a3c5d1f18910b3308cf0ee9f71819
Riyaz78609 <159205007+Riyaz78609@users.noreply.github.com>
2024-02-07 05:43:45 +0530

    hey I'm anime Tamil bot
    Vibe time...✨

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "21065203"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "aaea6aeafd738469288c5ed0d136ddd4" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "6943111067:AAHIRLurVYPkVYxGj5R3lnGscXd-p8xZqW8" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
